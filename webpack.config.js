116a3d2c46e5761172020b7ea5b068eed34e9c09
Nick Whitelegg <nickw4426@gmail.com>
2024-02-29 10:58:56 +0000

    Add Express server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './public/index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'public/dist'),
         filename: 'bundle.js'
